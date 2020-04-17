2868d43b4b356cb52c2149aa7fbc65dd81a0cbe7
Matt Graham <mpgraham917@gmail.com>
2020-04-16 18:23:52 -0700

    Finished Responsiveness

--- src/components/NavbarItems.js
+++ src/components/NavbarItems.js
@@ -1,43 +1,83 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { NavLink } from 'react-router-dom'
 import Modal from 'react-modal'
 
+import hamburgerSVG from '../assets/hamburger.svg'
 import SocialHorizontal from './SocialHorizontal'
 
 function NavbarItems() {
 	const [hamburgerExpanded, setHamburgerExpanded] = useState(false)
 	const [showResume, setShowResume] = useState(false)
+	const [iFrameLoading, setIFrameLoading] = useState(false)
+	const navItemsRef = useRef(null)
+
+	function handleClickOutside(event) {
+		if (navItemsRef.current && !navItemsRef.current.contains(event.target)) {
+			setHamburgerExpanded(false)
+		}
+	}
+
+	useEffect(() => {
+		if (!hamburgerExpanded) return
+		document.addEventListener('mousedown', handleClickOutside)
+		return () => {
+			document.removeEventListener('mousedown', handleClickOutside)
+		}
+	}, [navItemsRef, hamburgerExpanded])
 
 	const activeStyle = { fontWeight: 'bold' }
 	return (
 		<>
-			<ul className='horizontal-nav-items'>
-				<li><NavLink to='/projects' activeStyle={activeStyle}>Projects</NavLink></li>
-				<li><NavLink to='/experience' activeStyle={activeStyle}>Experience</NavLink></li>
-				<li><NavLink to='/contact' activeStyle={activeStyle}>Contact</NavLink></li>
-				<li><button type='button' onClick={() => setShowResume(true)}>Resume</button></li>
+			<button type='button'
+				className='nav-hamburger'
+				onClick={() => setHamburgerExpanded(!hamburgerExpanded)}
+			>
+				<img src={hamburgerSVG} />
+			</button>
+			<ul className={`nav-items ${hamburgerExpanded ? '' : 'hidden'}`} ref={navItemsRef}>
+				<li>
+					<NavLink to='/projects' activeStyle={activeStyle}
+						onClick={() => setHamburgerExpanded(false)}
+					>Projects</NavLink>
+				</li>
+				<li>
+					<NavLink to='/experience' activeStyle={activeStyle}
+						onClick={() => setHamburgerExpanded(false)}
+					>Experience</NavLink>
+				</li>
+				<li>
+					<NavLink to='/contact' activeStyle={activeStyle}
+						onClick={() => setHamburgerExpanded(false)}
+					>Contact</NavLink>
+				</li>
+				<li className='resume-li'>
+					<button type='button'
+						onClick={() => {
+							setHamburgerExpanded(false)
+							setIFrameLoading(true)
+							setShowResume(true)
+						}}
+					>Resume</button>
+				</li>
+				<li className='social'><SocialHorizontal /></li>
 			</ul>
-			<button type='button' onClick={() => setHamburgerExpanded(!hamburgerExpanded)} style={{ display: 'none' }}>Hamburger</button>
-			{hamburgerExpanded && <div className='vertical-nav-items'>
-				<NavLink to='/projects' activeStyle={activeStyle}>Projects</NavLink>
-				<NavLink to='/experience' activeStyle={activeStyle}>Experience</NavLink>
-				<NavLink to='/contact' activeStyle={activeStyle}>Contact</NavLink>
-				<li><button type='button' onClick={() => setShowResume(true)}>Resume</button></li>
-				<SocialHorizontal />
-			</div>}
+
 			<Modal
 				isOpen={showResume}
 				contentLabel='Resume Modal'
-				onRequestClose={() => setShowResume(false)}
+				onRequestClose={() => { setShowResume(false); setIFrameLoading(false) }}
 				shouldCloseOnOverlayClick={true}
 				className='resume-modal'
 				overlayClassName='resume-modal-overlay'
 				appElement={document.getElementById('app')}
 			>
 				<div className='actions'>
-					<button onClick={() => setShowResume(false)}>Close</button>
+					<button onClick={() => { setShowResume(false); setIFrameLoading(false) }}>Close</button>
 				</div>
-				<iframe src="https://drive.google.com/file/d/1Oql5F4Hd9t9YxQQFHRKvFOIuCa8HSaV8/preview"
+				{iFrameLoading && <p className='loading'>Loading...</p>}
+				<iframe
+					onLoad={() => setIFrameLoading(false)}
+					src="https://drive.google.com/file/d/1Oql5F4Hd9t9YxQQFHRKvFOIuCa8HSaV8/preview"
 				></iframe>
 			</Modal>
 		</>
