7917fe2e674649ddae0a7fcdeab0c72ce1df75b9
Matt G <mpgraham917@gmail.com>
2024-04-07 05:52:35 -0700

    Stop type checking node_modules

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import 'react18-json-view/src/dark.css';
 import 'react18-json-view/src/style.css';
 import App from './App';
 import './styles.scss';
-import './utils/overrides';
 
 export const container = document.getElementById('root')!;
 export const root = createRoot(container!);
--- src/utils/overrides.ts
+++
@@ -1,5 +0,0 @@
-const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-
-Date.prototype.toLocaleTimeString = function () {
-  return `${months[this.getMonth()]} ${this.getFullYear()}`;
-};
